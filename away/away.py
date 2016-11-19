9ff14cc9f0c58c63ac3ac52b0cd8d2548cfb6569
PaddoInWonderland <martijnrichardk@gmail.com>
2016-11-19 14:24:23 +0100

    Scrub the deck mate!

--- away/away.py
+++ away/away.py
@@ -22,7 +22,6 @@ class Away:
                     if data[author.id]['MESSAGE']:
                         em = discord.Embed(description=data[author.id]['MESSAGE'], color=discord.Color.blue())
                         em.set_author(name='{} is currently away'.format(author.display_name), icon_url=avatar)
-
                     else:
                         em = discord.Embed(color=discord.Color.blue())
                         em.set_author(name='{} is currently away'.format(author.display_name), icon_url=avatar)
@@ -39,7 +38,7 @@ class Away:
         else:
             data[context.message.author.id] = {}
             if len(str(message)) < 256:
-                data[context.message.author.id]['MESSAGE'] = " ".join(context.message.clean_content.split()[1:])
+                data[context.message.author.id]['MESSAGE'] = ' '.join(context.message.clean_content.split()[1:])
             else:
                 data[context.message.author.id]['MESSAGE'] = True
             msg = 'You\'re now set as away.'
