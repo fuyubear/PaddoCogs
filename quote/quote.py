6f39edb55cdd5b058ca2b87befea47598e64ab94
PaddoInWonderland <martijnrichardk@gmail.com>
2016-12-05 20:12:25 +0100

    Integer hotfix

--- quote/quote.py
+++ quote/quote.py
@@ -4,19 +4,19 @@ from discord.ext import commands
 
 class Quote:
     """
-    Quote someone with the message id
+    Quote someone with the message id. To get the message id you need to enable developer mode.
     """
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(pass_context=True, name='quote')
-    async def _q(self, context, message_id):
+    async def _q(self, context, message_id: int):
         """
-        Quote someone with the message id
+        Quote someone with the message id. To get the message id you need to enable developer mode.
         """
         channel = context.message.channel
         try:
-            message = await self.bot.get_message(channel, message_id)
+            message = await self.bot.get_message(channel, str(message_id))
             content = '\a\n'+message.clean_content
             author = message.author
             timestamp = message.timestamp.strftime('%d-%m-%y %H:%M:%S')
