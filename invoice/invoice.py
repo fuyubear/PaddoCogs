e9914bee70b3fbb52e2d4b0689746c6a5d4f35f1
PaddoInWonderland <martijnrichardk@gmail.com>
2017-01-23 13:40:07 +0100

    [invoice] It's in the 21st century now

--- invoice/invoice.py
+++ invoice/invoice.py
@@ -1,20 +1,23 @@
 import os
 from .utils import checks
 from discord.ext import commands
-from .utils.dataIO import fileIO
+from cogs.utils.dataIO import dataIO
 
 
 class InVoice:
     """Gives a custom to anyone who enters a voice channel. THIS ROLE MUST EXIST AND THE BOT MUST HAVE THE RIGHTS TO CHANGE ROLES FOR IT TO WORK!"""
     def __init__(self, bot):
         self.bot = bot
+        self.data = dataIO.json_load('data/invoice/settings.json')
 
-    async def listener(self, before, after):
+    async def _save_data(self):
+        dataIO.json_save('data/invoice/settings.json', self.data)
+
+    async def _on_voice_state_update(self, before, after):
         try:
             server = after.server
-            data = fileIO('data/invoice/settings.json', 'load')
-            if server.id in data:
-                server_role = data[server.id]['ROLE']
+            if server.id in self.data:
+                server_role = self.data[server.id]['ROLE']
                 if server_role:
                     for role in server.roles:
                         if role.name.lower() == server_role.lower():
@@ -29,14 +32,13 @@ class InVoice:
     @checks.mod_or_permissions(administrator=True)
     async def _invoicerole(self, context, role):
         """Set a role"""
-        data = fileIO('data/invoice/settings.json', 'load')
         server = context.message.server
         roles = [r.name.lower() for r in server.roles]
         if role.lower() in roles:
-            if server.id not in data:
-                data[server.id] = {}
-            data[server.id]['ROLE'] = role
-            fileIO('data/invoice/settings.json', 'save', data)
+            if server.id not in self.data:
+                self.data[server.id] = {}
+            self.data[server.id]['ROLE'] = role
+            await self._save_data()
             message = 'Role `{}` set for Invoice.'.format(role)
         else:
             message = 'Role `{}` does not exist on this server.'.format(role)
@@ -52,14 +54,14 @@ def check_folder():
 def check_file():
     data = {}
     f = "data/invoice/settings.json"
-    if not fileIO(f, "check"):
+    if not dataIO.is_valid_json(f):
         print("Creating default settings.json...")
-        fileIO(f, "save", data)
+        dataIO.save_json(f, data)
 
 
 def setup(bot):
     check_folder()
     check_file()
     n = InVoice(bot)
-    bot.add_listener(n.listener, 'on_voice_state_update')
+    bot.add_listener(n._on_voice_state_update, 'on_voice_state_update')
     bot.add_cog(n)
