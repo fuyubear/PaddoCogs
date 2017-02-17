6b8d465b452373ed533650b894bfdf494ca5f525
PaddoInWonderland <PaddoInWonderland@users.noreply.github.com>
2017-02-17 12:04:59 +0100

    [calculator] added sum

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -12,7 +12,7 @@ class Calculator:
     @commands.group(pass_context=True, name='calculator')
     async def _calc(self, context, *, m):
         m = ''.join(m)
-        math_filter = re.findall(r'[\[\]\-()*+/0-9=.,% ]|>|<|==|>=|<=|\||&|~|!=|^|range|random|randint|choice|randrange|True|False|if|and|or|else|is|not|for|in|acos|acosh|asin|asinh|atan|atan2|atanh|ceil|copysign|cos|cosh|degrees|e|erf|erfc|exp|expm1|fabs|factorial|floor|fmod|frexp|fsum|gamma|gcd|hypot|inf|isclose|isfinite|isinf|isnan|ldexp|lgamma|log|log10|log1p|log2|modf|nan|pi|pow|radians|sin|sinh|sqrt|tan|tanh|round', m)
+        math_filter = re.findall(r'[\[\]\-()*+/0-9=.,% ]|>|<|==|>=|<=|\||&|~|!=|^|sum|range|random|randint|choice|randrange|True|False|if|and|or|else|is|not|for|in|acos|acosh|asin|asinh|atan|atan2|atanh|ceil|copysign|cos|cosh|degrees|e|erf|erfc|exp|expm1|fabs|factorial|floor|fmod|frexp|fsum|gamma|gcd|hypot|inf|isclose|isfinite|isinf|isnan|ldexp|lgamma|log|log10|log1p|log2|modf|nan|pi|pow|radians|sin|sinh|sqrt|tan|tanh|round', m)
         print(''.join(math_filter))
         calculate_stuff = eval(''.join(math_filter))
         if len(str(calculate_stuff)) > 0:
