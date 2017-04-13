25b8d444180dfaa0a7402b21a6e0b940e42599f1
PaddoInWonderland <PaddoInWonderland@users.noreply.github.com>
2017-04-13 14:45:36 +0200

    [seen] bug fixes

--- seen/seen.py
+++ seen/seen.py
@@ -12,6 +12,38 @@ class Seen:
         self.seen = dataIO.load_json("data/seen/seen.json")
         self.new_data = False
 
+    async def _get_channel(self, id):
+        return discord.utils.get(self.bot.get_all_channels(), id=id)
+
+    async def data_writer(self):
+        while self == self.bot.get_cog("Seen"):
+            if self.new_data:
+                await asyncio.sleep(60)
+                print("Writing data to seen.json")
+                dataIO.save_json("data/seen/seen.json", self.seen)
+                self.new_data = False
+            else:
+                await asyncio.sleep(30)
+
+    @commands.command(pass_context=True, no_pm=True, name='seen')
+    async def _seen(self, context, username: discord.Member):
+        '''seen <@username>'''
+        server = context.message.server
+        author = username
+        if [author.id in self.seen[server.id] if server.id in self.seen else False]:
+            data = self.seen[server.id][author.id]
+            ts = data['TIMESTAMP']
+            last_message = data['MESSAGE']
+            channel = await self._get_channel(data['CHANNEL'])
+            em = discord.Embed(description='\a\n{}'.format(last_message), color=discord.Color.green())
+            avatar = author.avatar_url if author.avatar else author.default_avatar_url
+            em.set_author(name='{} was last seen on {} UTC'.format(author.display_name, ts), icon_url=avatar)
+            em.add_field(name='\a', value='**Channel:** #{}'.format(channel.name))
+            await self.bot.say(embed=em)
+        else:
+            message = 'I haven\'t seen {} yet.'.format(author.display_name)
+            await self.bot.say('{}'.format(message))
+
     async def listener(self, message):
         if not message.channel.is_private and self.bot.user.id != message.author.id:
             server = message.server
@@ -26,35 +58,6 @@ class Seen:
                 self.seen[server.id] = {}
             self.seen[server.id][author.id] = data
             self.new_data = True
-
-    @commands.command(pass_context=True, no_pm=True, name='seen')
-    async def _seen(self, context, username: discord.Member):
-        '''seen <@username>'''
-        server = context.message.server
-        author = username
-        if server.id in self.seen and author.id in self.seen[server.id]:
-            data = self.seen[server.id][author.id]
-            ts = data['TIMESTAMP']
-            last_message = data['MESSAGE']
-            channel = data['CHANNEL']
-            em = discord.Embed(description='\a\n{}'.format(last_message), color=discord.Color.green())
-            avatar = author.avatar_url if author.avatar else author.default_avatar_url
-            em.set_author(name='{} was last seen on {} UTC'.format(author.display_name, ts), icon_url=avatar)
-            em.add_field(name='\a', value='**Channel:** {}'.format(channel))
-            await self.bot.say(embed=em)
-        else:
-            message = 'I haven\'t seen {} yet.'.format(author.display_name)
-            await self.bot.say('{}'.format(message))
-
-    async def data_writer(self):
-        while self == self.bot.get_cog("Seen"):
-            if self.new_data:
-                await asyncio.sleep(60)
-                print("Writing data to seen.json")
-                dataIO.save_json("data/seen/seen.json", self.seen)
-                self.new_data = False
-            else:
-                await asyncio.sleep(30)
 
 
 def check_folder():
