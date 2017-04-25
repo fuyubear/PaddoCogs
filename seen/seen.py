2c3f156cf18cab01ec6b7e0d389a176c5ecc9ce0
PaddoInWonderland <PaddoInWonderland@users.noreply.github.com>
2017-04-25 14:40:50 +0200

    [seen] cosmetic and database changes - resets old database.

--- seen/seen.py
+++ seen/seen.py
@@ -3,24 +3,25 @@ from cogs.utils.dataIO import dataIO
 import discord
 import os
 import asyncio
+from datetime import datetime
+
+
+DB_VERSION = 2
 
 
 class Seen:
     '''Check when someone was last seen.'''
     def __init__(self, bot):
         self.bot = bot
-        self.seen = dataIO.load_json("data/seen/seen.json")
+        self.seen = dataIO.load_json('data/seen/seen.json')
         self.new_data = False
 
-    async def _get_channel(self, id):
-        return discord.utils.get(self.bot.get_all_channels(), id=id)
-
     async def data_writer(self):
-        while self == self.bot.get_cog("Seen"):
+        while self == self.bot.get_cog('Seen'):
             if self.new_data:
-                await asyncio.sleep(60)
-                dataIO.save_json("data/seen/seen.json", self.seen)
+                dataIO.save_json('data/seen/seen.json', self.seen)
                 self.new_data = False
+                await asyncio.sleep(60)
             else:
                 await asyncio.sleep(30)
 
@@ -29,32 +30,52 @@ class Seen:
         '''seen <@username>'''
         server = context.message.server
         author = username
-        print(True if author.id in self.seen[server.id] else False if server.id in self.seen else False)
+        timestamp_now = context.message.timestamp
         if True if author.id in self.seen[server.id] else False if server.id in self.seen else False:
             data = self.seen[server.id][author.id]
-            ts = data['TIMESTAMP']
-            channel = await self._get_channel(data['CHANNEL'])
+            timestamp_then = datetime.fromtimestamp(data['TIMESTAMP'])
+            timestamp = timestamp_now - timestamp_then
+            days = timestamp.days
+            seconds = timestamp.seconds
+            hours = seconds // 3600
+            seconds = seconds - (hours * 3600)
+            minutes = seconds // 60
+            if sum([days, hours, minutes]) < 1:
+                ts = 'just now'
+            else:
+                ts = ''
+                if days == 1:
+                    ts += '{} day, '.format(days)
+                elif days > 1:
+                    ts += '{} days, '.format(days)
+                if hours == 1:
+                    ts += '{} hour, '.format(hours)
+                elif hours > 1:
+                    ts += '{} hours, '.format(hours)
+                if minutes == 1:
+                    ts += '{} minute ago'.format(minutes)
+                elif minutes > 1:
+                    ts += '{} minutes ago'.format(minutes)
             em = discord.Embed(color=discord.Color.green())
             avatar = author.avatar_url if author.avatar else author.default_avatar_url
-            em.set_author(name='{} was last seen on {} UTC in #{}'.format(author.display_name, ts, channel.name), icon_url=avatar)
+            em.set_author(name='{} was seen {}'.format(author.display_name, ts), icon_url=avatar)
             await self.bot.say(embed=em)
         else:
             message = 'I haven\'t seen {} yet.'.format(author.display_name)
             await self.bot.say('{}'.format(message))
 
-    async def listener(self, message):
+    async def on_message(self, message):
         if not message.channel.is_private and self.bot.user.id != message.author.id:
-            server = message.server
-            channel = message.channel
-            author = message.author
-            ts = message.timestamp
-            data = {}
-            data['TIMESTAMP'] = '{} {}:{}:{}'.format(ts.date(), ts.hour, ts.minute, ts.second)
-            data['CHANNEL'] = channel.id
-            if server.id not in self.seen:
-                self.seen[server.id] = {}
-            self.seen[server.id][author.id] = data
-            self.new_data = True
+            if not any(message.content.startswith(n) for n in self.bot.settings.prefixes):
+                server = message.server
+                author = message.author
+                ts = message.timestamp.timestamp()
+                data = {}
+                data['TIMESTAMP'] = ts
+                if server.id not in self.seen:
+                    self.seen[server.id] = {}
+                self.seen[server.id][author.id] = data
+                self.new_data = True
 
 
 def check_folder():
@@ -64,16 +85,31 @@ def check_folder():
 
 
 def check_file():
-    if not dataIO.is_valid_json("data/seen/seen.json"):
-        print("Creating seen.json...")
-        dataIO.save_json("data/seen/seen.json", {})
+    data = {}
+    data['db_version'] = DB_VERSION
+    f = 'data/seen/seen.json'
+    if not dataIO.is_valid_json(f):
+        print('Creating seen.json...')
+        dataIO.save_json(f, data)
+    else:
+        check = dataIO.load_json(f)
+        if 'db_version' in check:
+            if check['db_version'] < DB_VERSION:
+                data = {}
+                data['db_version'] = DB_VERSION
+                dataIO.save_json(f, data)
+                print('SEEN: Database version too old, resetting!')
+        else:
+            data = {}
+            data['db_version'] = DB_VERSION
+            dataIO.save_json(f, data)
+            print('SEEN: Database version too old, resetting!')
 
 
 def setup(bot):
     check_folder()
     check_file()
     n = Seen(bot)
-    bot.add_listener(n.listener, 'on_message')
     loop = asyncio.get_event_loop()
     loop.create_task(n.data_writer())
     bot.add_cog(n)
