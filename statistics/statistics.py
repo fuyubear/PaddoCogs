433301c4ca7ec8dcd22b3f8f4a4067e041c351e6
PaddoInWonderland <PaddoInWonderland@users.noreply.github.com>
2017-07-09 22:58:09 +0200

    [statistics] Some optimization for future projects

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -1,8 +1,8 @@
-
 from discord.ext import commands
 from __main__ import send_cmd_help
 from .utils.dataIO import dataIO
 from .utils import checks
+import collections
 import datetime
 import asyncio
 import discord
@@ -29,7 +29,7 @@ class Statistics:
         """
         Retreive statistics
         """
-        message = await self.retrieve_statistics()
+        message = await self.embed_statistics()
         await self.bot.say(embed=message)
 
     @commands.command(pass_context=True)
@@ -95,7 +95,39 @@ class Statistics:
         message = 'Channel cleared'
         await self.bot.say(message)
 
-    async def retrieve_statistics(self):
+    async def embed_statistics(self):
+        stats = self.retrieve_statistics()
+        em = discord.Embed(description='\a\n', color=discord.Color.red())
+        avatar = self.bot.user.avatar_url if self.bot.user.avatar else self.bot.user.default_avatar_url
+        em.set_author(name='Statistics of {}'.format(stats.name), icon_url=avatar)
+
+        em.add_field(name='**Uptime**', value='{}'.format(self.get_bot_uptime(brief=True)))
+
+        em.add_field(name='**Users**', value=stats.users)
+        em.add_field(name='**Servers**', value=stats.servers)
+
+        em.add_field(name='**Channels**', value=str(stats.channels))
+        em.add_field(name='**Text channels**', value=str(stats.text_channels))
+        em.add_field(name='**Voice channels**', value=str(stats.voice_channels))
+
+        em.add_field(name='**Messages received**',
+                     value=str(stats.read_messages))
+        em.add_field(name='**Commands run**', value=str(stats.commands_run))
+        em.add_field(name='\a', value='\a')
+
+        em.add_field(name='**Active cogs**', value=str(len(self.bot.cogs)))
+        em.add_field(name='**Commands**', value=str(len(self.bot.commands)))
+        em.add_field(name='\a', value='\a')
+
+        em.add_field(name='\a', value='\a', inline=False)
+        em.add_field(name='**CPU usage**', value='{0:.1f}%'.format(stats.cpu_usage))
+        em.add_field(name='**Memory usage**', value='{0:.1f}%'.format(stats.mem_v))
+
+        em.add_field(name='\a', value='\a')
+        em.set_footer(text='API version {}'.format(discord.__version__))
+        return em
+
+    def retrieve_statistics(self):
         name = self.bot.user.name
         users = str(len(set(self.bot.get_all_members())))
         servers = str(len(self.bot.servers))
@@ -107,7 +139,7 @@ class Statistics:
         cpu_p = psutil.cpu_percent(interval=None, percpu=True)
         cpu_usage = sum(cpu_p) / len(cpu_p)
 
-        mem_v = psutil.virtual_memory()
+        mem_v = psutil.virtual_memory().percent
 
         for channel in self.bot.get_all_channels():
             if channel.type == discord.ChannelType.text:
@@ -116,35 +148,17 @@ class Statistics:
                 voice_channels += 1
         channels = text_channels + voice_channels
 
-        em = discord.Embed(description='\a\n', color=discord.Color.red())
-        avatar = self.bot.user.avatar_url if self.bot.user.avatar else self.bot.user.default_avatar_url
-        em.set_author(name='Statistics of {}'.format(name), icon_url=avatar)
-
-        em.add_field(name='**Uptime**', value='{}'.format(self.get_bot_uptime(brief=True)))
-
-        em.add_field(name='**Users**', value=users)
-        em.add_field(name='**Servers**', value=servers)
-
-        em.add_field(name='**Channels**', value=str(channels))
-        em.add_field(name='**Text channels**', value=str(text_channels))
-        em.add_field(name='**Voice channels**', value=str(voice_channels))
-
-        em.add_field(name='**Messages received**',
-                     value=str(read_messages))
-        em.add_field(name='**Commands run**', value=str(commands_run))
-        em.add_field(name='\a', value='\a')
-
-        em.add_field(name='**Active cogs**', value=str(len(self.bot.cogs)))
-        em.add_field(name='**Commands**', value=str(len(self.bot.commands)))
-        em.add_field(name='\a', value='\a')
-
-        em.add_field(name='\a', value='\a', inline=False)
-        em.add_field(name='**CPU usage**', value='{0:.1f}%'.format(cpu_usage))
-        em.add_field(name='**Memory usage**', value='{0:.1f}%'.format(mem_v.percent))
-
-        em.add_field(name='\a', value='\a')
-        em.set_footer(text='API version {}'.format(discord.__version__))
-        return em
+        stats = collections.namedtuple(
+                'stats',
+                ['name', 'users', 'servers', 'commands_run', 'read_messages', 'text_channels', 'voice_channels', 'channels', 'cpu_usage', 'mem_v'],
+                verbose=False, rename=False
+                )
+        return stats(
+                name=name, users=users, servers=servers, commands_run=commands_run,
+                read_messages=read_messages, text_channels=text_channels,
+                voice_channels=voice_channels, channels=channels,
+                cpu_usage=cpu_usage, mem_v=mem_v
+                )
 
     def get_bot_uptime(self, *, brief=False):
         # Stolen from owner.py - Courtesy of Danny
@@ -169,20 +183,23 @@ class Statistics:
     async def reload_stats(self):
         await asyncio.sleep(30)
         while self == self.bot.get_cog('Statistics'):
-            if self.settings['CHANNEL_ID']:
-                msg = await self.retrieve_statistics()
-                channel = discord.utils.get(
-                    self.bot.get_all_channels(), id=self.settings['CHANNEL_ID'])
-                messages = False
-                async for message in self.bot.logs_from(channel, limit=1):
-                    messages = True
-                    if message.author.name == self.bot.user.name:
-                        await self.bot.edit_message(message, embed=msg)
-                if not messages:
-                    await self.bot.send_message(channel, embed=msg)
-            else:
+            try:
+                if self.settings['CHANNEL_ID']:
+                    msg = await self.embed_statistics()
+                    channel = discord.utils.get(
+                        self.bot.get_all_channels(), id=self.settings['CHANNEL_ID'])
+                    messages = False
+                    async for message in self.bot.logs_from(channel, limit=1):
+                        messages = True
+                        if message.author.name == self.bot.user.name:
+                            await self.bot.edit_message(message, embed=msg)
+                    if not messages:
+                        await self.bot.send_message(channel, embed=msg)
+                else:
+                    pass
+                await asyncio.sleep(self.refresh_rate)
+            except:
                 pass
-            await asyncio.sleep(self.refresh_rate)
 
 
 def check_folder():
