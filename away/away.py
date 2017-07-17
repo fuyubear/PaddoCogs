7f49a79fe9e969db37e5ebefce6eae061e4a5e4e
PaddoInWonderland <PaddoInWonderland@users.noreply.github.com>
2017-07-17 11:57:42 +0200

    [away] opt-out for servers

--- away/away.py
+++ away/away.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from .utils import checks
 from discord.ext import commands
 from cogs.utils.dataIO import dataIO
 
@@ -12,26 +13,28 @@ class Away:
 
     async def listener(self, message):
         tmp = {}
-        for mention in message.mentions:
-            tmp[mention] = True
-        if message.author.id != self.bot.user.id:
-            for author in tmp:
-                if author.id in self.data:
-                    try:
-                        avatar = author.avatar_url if author.avatar else author.default_avatar_url
-                        if self.data[author.id]['MESSAGE']:
-                            em = discord.Embed(description=self.data[author.id]['MESSAGE'], color=discord.Color.blue())
-                            em.set_author(name='{} is currently away'.format(author.display_name), icon_url=avatar)
-                        else:
-                            em = discord.Embed(color=discord.Color.blue())
-                            em.set_author(name='{} is currently away'.format(author.display_name), icon_url=avatar)
-                        await self.bot.send_message(message.channel, embed=em)
-                    except:
-                        if self.data[author.id]['MESSAGE']:
-                            msg = '{} is currently away and has set the following message: `{}`'.format(author.display_name, self.data[author.id]['MESSAGE'])
-                        else:
-                            msg = '{} is currently away'.format(author.display_name)
-                        await self.bot.send_message(message.channel, msg)
+        server = message.server
+        if server.id not in self.data:
+            for mention in message.mentions:
+                tmp[mention] = True
+            if message.author.id != self.bot.user.id:
+                for author in tmp:
+                    if author.id in self.data:
+                        try:
+                            avatar = author.avatar_url if author.avatar else author.default_avatar_url
+                            if self.data[author.id]['MESSAGE']:
+                                em = discord.Embed(description=self.data[author.id]['MESSAGE'], color=discord.Color.blue())
+                                em.set_author(name='{} is currently away'.format(author.display_name), icon_url=avatar)
+                            else:
+                                em = discord.Embed(color=discord.Color.blue())
+                                em.set_author(name='{} is currently away'.format(author.display_name), icon_url=avatar)
+                            await self.bot.send_message(message.channel, embed=em)
+                        except:
+                            if self.data[author.id]['MESSAGE']:
+                                msg = '{} is currently away and has set the following message: `{}`'.format(author.display_name, self.data[author.id]['MESSAGE'])
+                            else:
+                                msg = '{} is currently away'.format(author.display_name)
+                            await self.bot.send_message(message.channel, msg)
 
     @commands.command(pass_context=True, name="away")
     async def _away(self, context, *message: str):
@@ -49,6 +52,19 @@ class Away:
             msg = 'You\'re now set as away.'
         dataIO.save_json('data/away/away.json', self.data)
         await self.bot.say(msg)
+
+    @commands.command(pass_context=True, name="toggleaway")
+    @checks.mod_or_permissions(administrator=True)
+    async def _ignore(self, context):
+        server = context.message.server
+        if server.id in self.data:
+            del self.data[server.id]
+            message = 'Not ignoring this server anymore.'
+        else:
+            self.data[server.id] = True
+            message = 'Ignoring this server.'
+        dataIO.save_json('data/away/away.json', self.data)
+        await self.bot.say(message)
 
 
 def check_folder():
